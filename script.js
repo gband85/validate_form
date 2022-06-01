52936586ff13bdf90b4ddf6ce0d0046d0a6b8ee0
gband85 <garrett@gband.net>
2022-06-01 18:49:01 -0500

    add form validation

--- script.js
+++ script.js
@@ -11,14 +11,39 @@ const zip=document.querySelector('#zip');
 const password=document.querySelector('#password');
 const password2=document.querySelector('#password2');
 
-email.addEventListener("input",function(){
-    if (email.validity.valid) {
+const formFields=document.querySelectorAll('input');
+formFields.forEach((field)=>{
+    if (field.id!="password2") {
+    field.addEventListener('input',()=>{
+if (field.validity.valid) {
+    field.nextElementSibling.textContent="";
+}
+else {
+showError(field);
+}
+    })
+}
+})
 
+password2.addEventListener("input",()=>{
+    if (password2.value!=password.value) {
+password2.nextElementSibling.textContent="Passwords do not match";
+password2.style.backgroundColor="#fdd";
     }
     else {
-        showError();
+    password2.nextElementSibling.textContent="";
+    password2.style.backgroundColor="#fff"
     }
 })
+// email.addEventListener("input",function(){
+//     console.log(email.id)
+//     if (email.validity.valid) {
+
+//     }
+//     else {
+//         showError(email);
+//     }
+// })
 
 form.addEventListener('submit',function(e) {
     e.preventDefault();
@@ -26,20 +51,37 @@ form.addEventListener('submit',function(e) {
 })
 
 function showError(field){
-// switch(field) {
-    // case email: 
-    if (email.validity.valueMissing) {
-emailError.textContent="Enter a value."
+       if (field.validity.valueMissing) {
+field.nextElementSibling.textContent=`Enter ${field.id}.`;
     }
-    else if (email.validity.typeMismatch) {
-emailError.textContent="Enter an email address."
+
+    else {
+ switch(field.id) {
+     case "email": 
+ 
+    if (field.validity.typeMismatch) {
+        field.nextElementSibling.textContent="Enter a valid email address."
     }
-    else if (email.validity.tooShort) {
-        emailError.textContent="Email too short";
-    }
-    // case country:
-    // case zip:
-    // case password:
-    // case password2:
+    else if (field.validity.tooShort) {
+        field.nextElementSibling.textContent=`Email address too short. 8 characters minimum.`;
+}
+      case "country":
+          if (field.validity.tooShort) {
+            field.nextElementSibling.textContent="Country name too short."
+          }
+ case "zip":
+     if (field.validity.badInput) {
+    field.nextElementSibling.textContent="Numbers only. Please enter a valid zip code."
+     }
+     else if (field.validity.rangeOverflow || field.validity.rangeUnderflow) {
+        field.nextElementSibling.textContent=`Please enter a valid zip code.`;
+}
+     case "password":
+if (field.validity.patternMismatch) {
+    field.nextElementSibling.textContent="Password does not fulfill minimum security requirements."
+}
+
 // }
+}
+    }
 }
